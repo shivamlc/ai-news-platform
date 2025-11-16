8ea277f4dde0ffc67c6eae00df6e3cce271b7ac6
shivamacshack <s.gaur@uq.edu.au>
2025-11-16 19:13:37 +1000

    Initial commit

---
+++ src/test/java/com/sg_tech/ai_news_platform/AiNewsPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sg_tech.ai_news_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiNewsPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
