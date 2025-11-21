2e5ee32461a18097137ba89ba432157c1d2fc92a
shivamacshack <s.gaur@uq.edu.au>
2025-11-21 22:53:42 +1000

    added hookdeck support to bash script

--- run_app_default.sh
+++ run_app_default.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 # Remove local config server image
 
+echo "Stopping and removing existing containers..."
+docker compose -f docker-compose/default/docker-compose.yml down
+
 docker rmi sgaurtech/config_server:v1 || true
 
 echo "Building new Docker image..."
 docker build -t sgaurtech/config_server:v1 ./config_server
 
-echo "Stopping and removing existing containers..."
-docker compose -f docker-compose/default/docker-compose.yml down
-
 echo "Starting containers..."
 docker compose -f docker-compose/default/docker-compose.yml up -d
+
+echo "Logging out of Hookdeck..."
+hookdeck logout || true
+
+echo "Logging in to Hookdeck..."
+hookdeck login
+
+echo "Starting Hookdeck listener..."
+hookdeck listen 8071 ai-news-platform-config
 
 echo "Done."
